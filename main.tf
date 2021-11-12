d389ecfc0aee03cec1a3c4523b9c698b7b2db06a
vedant-mhatre <vedantmh@gmail.com>
2021-11-12 13:10:31 +0530

    minor changes

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "public-web-ec2" {
   availability_zone           = "ap-south-1a"
   key_name                    = "Task"
   subnet_id                   = "subnet-019d14a13c07a30c5"
-  vpc_security_group_ids      = "sg-0730e1c4f2a1c04fd"
+  vpc_security_group_ids      = {"sg-0730e1c4f2a1c04fd"}
   associate_public_ip_address = true
 
 }
